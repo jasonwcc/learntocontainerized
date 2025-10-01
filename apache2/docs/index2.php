09e4a860446a27f5cc7b4ca2e0bb628365213c5c
jasonwcc <jasonwcc@yahoo.com>
2025-10-01 01:49:26 +0000

    changes

--- apache2/docs/index2.php
+++ apache2/docs/index2.php
@@ -1,13 +1,15 @@
 <?php
 $client_ip = $_SERVER['REMOTE_ADDR'];
 $client_port = $_SERVER['REMOTE_PORT'];
+$server_ip = getenv("REMOTE_ADDR");
 
-echo "Welcome to Jaz Web server 1.0.0.1\n\n";
-echo "This image is built with :\n";
-echo "Container: docker.io/library/php:7.2-apache\n";
-echo "Codes    : https://github.com/jasonwcc/learntocontainerized\n";
-echo "Hostname : " . gethostname() . "\n";
-echo "Server IP: " . $client_ip . "\n";
-echo "Port     : " . $client_port . "\n\n";
-echo "Created for training purposes by J\n"
+echo "Welcome to Jaz Web server 1.0.0.1<br><br>";
+echo "This image is built with :<br>";
+echo "Container: docker.io/library/php:7.2-apache<br>";
+echo "Codes    : https://github.com/jasonwcc/learntocontainerized<br>";
+echo "Hostname : " . gethostname() . "<br>";
+echo "Server IP: " . $server_ip . "<br>";
+echo "Client IP: " . $client_ip . "<br>";
+echo "Port     : " . $client_port . "<br><br>";
+echo "Created for training purposes by J<br>"
 ?>
