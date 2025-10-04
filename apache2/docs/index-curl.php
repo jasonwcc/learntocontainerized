c56863283d6e49fa8a4e2bc694fb51439b37ce6c
jas <j@g.com>
2025-10-04 04:01:44 +0000

    changes 20251004

--- apache2/docs/index-curl.php
+++ apache2/docs/index-curl.php
@@ -1,15 +1,17 @@
 <?php
-$client_ip = $_SERVER['REMOTE_ADDR'];
-$client_port = $_SERVER['REMOTE_PORT'];
-$server_ip = getenv("REMOTE_ADDR");
+ = ['REMOTE_ADDR'];
+ = ['REMOTE_PORT'];
+ = ['SERVER_ADDR'];
+ = ['SERVER_PORT'];
 
-echo "Welcome to Jaz Web server 1.0.0.1\n\n";
+echo "Welcome to J Web server 1.0.9.9\n\n";
 echo "This image is built with :\n";
-echo "Container: docker.io/library/php:7.2-apache\n";
-echo "Codes    : https://github.com/jasonwcc/learntocontainerized\n";
-echo "Hostname : " . gethostname() . "\n";
-echo "Server IP: " . $server_ip . "\n";
-echo "Client IP: " . $client_ip . "\n";
-echo "Port     : " . $client_port . "\n\n";
+echo "Container  : docker.io/library/php:7.2-apache\n";
+echo "Codes      : https://github.com/jasonwcc/learntocontainerized\n";
+echo "Hostname   : " . gethostname() . "\n";
+echo "Server IP  : " .  . "\n";
+echo "Client IP  : " .  . "\n";
+echo "Server Port: " .  . "\n";
+echo "Client Port: " .  . "\n\n";
 echo "Created for training purposes by J\n"
 ?>
--- apache2/docs/index.php
+++ apache2/docs/index.php
@@ -1,17 +1,17 @@
 <?php
-$client_ip = $_SERVER['REMOTE_ADDR'];
-$client_port = $_SERVER['REMOTE_PORT'];
-$server_ip = getenv("REMOTE_ADDR");
-$http_client_ip = getenv('HTTP_CLIENT_IP');
+ = ['REMOTE_ADDR'];
+ = ['REMOTE_PORT'];
+ = getenv("REMOTE_ADDR");
+ = ['SERVER_PORT'];
 
 echo "Welcome to Jaz Web server 1.0.0.1<br><br>";
 echo "This image is built with :<br>";
 echo "Container: docker.io/library/php:7.2-apache<br>";
-echo "Codes    : https://github.com/jasonwcc/learntocontainerized<br>";
-echo "Hostname : " . gethostname() . "<br>";
-echo "Server IP: " . $server_ip . "<br>";
-echo "Client IP: " . $client_ip . "<br>";
-echo "HTTP client IP: " . $http_client_ip . "<br>";
-echo "Port     : " . $client_port . "<br><br>";
-echo "Created for training purposes by J<br>"
+echo "Codes      : https://github.com/jasonwcc/learntocontainerized<br>";
+echo "Hostname   : " . gethostname() . "<br>";
+echo "Server IP  : " .  . "<br>";
+echo "Client IP  : " .  . "<br>";
+echo "Server Port: " .  . "<br>";
+echo "Client Port: " .  . "<br><br>";
+echo "Created for training purposes by J"
 ?>
