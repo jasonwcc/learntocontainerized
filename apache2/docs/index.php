a77abfadee2277227ff411e406f8204027d70600
jas <j@g.com>
2025-10-04 04:04:36 +0000

    changes 20251004

--- apache2/docs/index.php
+++ apache2/docs/index.php
@@ -1,17 +1,18 @@
 <?php
- = ['REMOTE_ADDR'];
- = ['REMOTE_PORT'];
- = getenv("REMOTE_ADDR");
- = ['SERVER_PORT'];
+$client_ip = $_SERVER['REMOTE_ADDR'];
+$client_port = $_SERVER['REMOTE_PORT'];
+$server_ip = getenv("REMOTE_ADDR");
+$server_port = $_SERVER['SERVER_PORT'];
 
 echo "Welcome to Jaz Web server 1.0.0.1<br><br>";
 echo "This image is built with :<br>";
 echo "Container: docker.io/library/php:7.2-apache<br>";
 echo "Codes      : https://github.com/jasonwcc/learntocontainerized<br>";
 echo "Hostname   : " . gethostname() . "<br>";
-echo "Server IP  : " .  . "<br>";
-echo "Client IP  : " .  . "<br>";
-echo "Server Port: " .  . "<br>";
-echo "Client Port: " .  . "<br><br>";
+echo "Server IP  : " . $server_ip . "<br>";
+echo "Client IP  : " . $client_ip . "<br>";
+echo "Server Port: " . $server_port . "<br>";
+echo "Client Port: " . $client_port . "<br><br>";
 echo "Created for training purposes by J"
 ?>
+
